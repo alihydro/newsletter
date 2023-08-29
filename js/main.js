30e0a11ed3967cd1210d0bf053a9ef13030a81f3
saidi ali <saidialidz9@gmail.com>
2023-08-29 14:45:59 +0100

    UpLoad newsletter-sign-up

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const emailInput = document.querySelector('input[type="text"]');
+    const formTextError = document.querySelector('.form-text h1');
+    const subscribeButton = document.querySelector('button');
+    const confirmationEmailSpan = document.getElementById('confirmationEmail');
+ 
+
+
+    emailInput.addEventListener("blur", function () {
+      const emailValue = emailInput.value.trim();
+      if (!emailValue.includes("@")) {
+        formTextError.style.display = "block";
+        emailInput.style.color = "hsl(4, 100%, 67%)"; 
+        emailInput.style.borderColor = "hsl(4, 100%, 67%)"; 
+        emailInput.style.backgroundColor = "rgb(255 98 87 / 16%)";
+
+      } else {
+        formTextError.style.display = "none";
+      }
+    });
+    subscribeButton.addEventListener("click", function () {
+      const emailValue = emailInput.value.trim();
+      if (emailValue.includes("@")) {
+        window.location.href = "http://127.0.0.1:5500/index%20(2).html";
+      }
+    });
+    
+  });
